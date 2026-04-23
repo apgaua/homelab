e9f791e3bf813b0d5e9e70f8e5fb58286a02ad07
Apgaua <apgaua@pm.me>
2026-04-23 17:59:55 -0300

    refactor: consolidate ArgoCD repository secret definition into resource_argocd.tf

--- deploy_talos_cluster/resource_argocd.tf
+++ deploy_talos_cluster/resource_argocd.tf
@@ -76,3 +76,26 @@ resource "argocd_application" "applications" {
   cascade    = false
   depends_on = [helm_release.argocd, kubernetes_secret_v1.argocd_repo_secret]
 }
+
+resource "kubernetes_secret_v1" "argocd_repo_secret" {
+  metadata {
+    name      = "apgaua-repo-secret"
+    namespace = "argocd"
+
+    # This label is critical; it tells ArgoCD to use this secret for repository credentials
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repository"
+    }
+  }
+
+  # The Terraform Kubernetes provider automatically base64-encodes the values in the 'data' block
+  data = {
+    type     = "git"
+    url      = var.applications[0].repo_url
+    username = var.github.username
+    password = var.github.token
+  }
+
+  type       = "Opaque"
+  depends_on = [null_resource.waiting, helm_release.argocd]
+}
